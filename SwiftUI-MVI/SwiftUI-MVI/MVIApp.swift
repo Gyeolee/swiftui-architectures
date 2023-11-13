01b25ed292bfe236608517b8a436d45aa159f534
Gyeolee <gyeoleeeeeee@gmail.com>
2023-11-13 10:04:46 +0900

    [ADD] [MVI] 프로젝트 생성

---
+++ SwiftUI-MVI/SwiftUI-MVI/MVIApp.swift
@@ -0,0 +1,17 @@
+//
+//  MVIApp.swift
+//  SwiftUI-MVI
+//
+//  Created by Hangyeol on 11/10/23.
+//
+
+import SwiftUI
+
+@main
+struct MVIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            RootView()
+        }
+    }
+}
