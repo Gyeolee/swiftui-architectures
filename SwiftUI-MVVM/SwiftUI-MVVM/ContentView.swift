a6379013a56cd39ce7b67bba3441fc1d00135d38
Gyeolee <gyeoleeeeeee@gmail.com>
2023-11-15 14:50:30 +0900

    [ADD] [MVVM] 프로젝트 생성

---
+++ SwiftUI-MVVM/SwiftUI-MVVM/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SwiftUI-MVVM
+//
+//  Created by Hangyeol on 11/15/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SwiftUI-MVVM/SwiftUI-MVVM/MVVMApp.swift
@@ -0,0 +1,17 @@
+//
+//  MVVMApp.swift
+//  SwiftUI-MVVM
+//
+//  Created by Hangyeol on 11/15/23.
+//
+
+import SwiftUI
+
+@main
+struct MVVMApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
